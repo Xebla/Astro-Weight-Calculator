e7b6013a9db5308963bfbb2fea4ba29d89391036
Xebla <dillonhuy102@gmail.com>
2025-04-23 09:19:36 -0700

    Followed instructions to complete Astro Weight Calculator

--- main.js
+++ main.js
@@ -1,1 +1,61 @@
-// Write your JavaScript code here!
+// Given Array of planets with respective weight multiplier
+var planets = [    
+    ['Pluto', 0.06], 
+    ['Neptune', 1.148], 
+    ['Uranus', 0.917], 
+    ['Saturn', 1.139], 
+    ['Jupiter', 2.640], 
+    ['Mars', 0.3895], 
+    ['Moon', 0.1655], 
+    ['Earth', 1], 
+    ['Venus', 0.9032], 
+    ['Mercury', 0.377], 
+    ['Sun', 27.9] 
+];      
+// Get dropdown element from HTML element with the id "planets"
+const dropdown = document.getElementById("planets") 
+
+// Loop through each item in the array
+planets.forEach(planet => {
+    //Create new <option> element to add to dropdown
+    const option = document.createElement("option");
+    // Set 'value' to be the multiplier portion of the option
+    option.value = planet[1];
+    // Set visible text to be taken from index 0 of sub-arrays
+    option.text = planet[0];
+    // Add the <option> element to the dropdown menu
+    dropdown.appendChild(option);
+});
+
+function calculateWeight(weight, planetName) { 
+    // 2. Write the code to return the correct weight 
+    for (let i = 0; i < planets.length; i++) {
+        const name = planets[i][0];         // Planet name
+        const gravity = planets[i][1];      // Planet gravity
+        // User selects planet
+        if (name === planetName) {
+            // Calculate and return new weight 
+            return weight * gravity;
+        }
+    }
+} 
+
+
+function handleClickEvent(e) {
+    // Store user input weight in variable "userWeight" and converts string into a number 
+    const userWeight = parseFloat(document.getElementById("user-weight").value);
+    // Set planetName to planet chosen by user from dropdown options 
+    const planetName = dropdown.options[dropdown.selectedIndex].text;
+    // Calls previous function and stores value inside of "reselt"
+    let result = calculateWeight(userWeight, planetName);
+    // Clean up unnecessary decimals with Number()
+   const cleanResult = Number(result.toFixed(2));
+    // Get result text
+    const resultText = document.getElementById("result");
+   return resultText.textContent = `If you were on ${planetName}, you would weigh ${cleanResult} lbs!`;
+} 
+
+    // feel free to add classes and id's to the HTML elements as you need, 
+
+    // Bonus Challenges 
+    // 8. Reverse the drop down order so that the sun is first.
